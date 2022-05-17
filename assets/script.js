9211d911022c2cea75236ef3822fbebf5342172d
Nate Wolfe <wolfe.nate26@gmail.com>
2022-05-16 19:46:57 -0600

    made the col and rows, added buttons and time

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var today = moment().format("LLLL");
+$("#currentDay").text(today);
+
+//save button on the right side in the last column. saves to local storage.
+
+// change the past hours to their specific colors
+// current hour should be its specific color
+// future hours will be their color.
+// need to make the comparison of moment to determine.
+
+$(".btn").on("click", function () {
+  var buttonid = $(this).attr("id");
+  console.log(buttonid);
+});
